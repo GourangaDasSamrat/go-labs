c7988bf3acfc71fb064be2229a430f85cc42cc19
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-08 20:36:31 +0600

    feat(pointers): add basic pointer usage examples
    - Demonstrate pointer declaration
    - Show default nil pointer value
    - Access value through dereferencing
    - Modify value using pointer

--- 07_Memory-Management/main.go
+++ 07_Memory-Management/main.go
@@ -7,4 +7,16 @@ import (
 
 func main() {
 	fmt.Println("Logical CPUs:", runtime.NumCPU())
+
+	var userName *string
+	fmt.Println("Default value of pointer is ", userName)
+
+	myAge := 17
+
+	var ptr = &myAge
+	fmt.Println("Memory address of pointer is: ", ptr)
+	fmt.Println("Actual value of pointer is: ", *ptr)
+
+	*ptr = *ptr * 2
+	fmt.Println("My new age is: ", myAge) // i'm not this much old 😂️
 }
