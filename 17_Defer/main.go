0367c4fc04dcc5efc073ede7dfe14eea7de10933
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-17 18:01:28 +0600

    feat(defer): demonstrate LIFO execution order
    - Added comments explaining LIFO
    - Added example of defer in a loop
    - Each loop iteration defers a call
    - Deferred calls execute after function return

--- 17_Defer/main.go
+++ 17_Defer/main.go
@@ -5,17 +5,26 @@ import (
 )
 
 func main() {
-	defer fmt.Println("I'm defer 1")
+	// Deferred statements are executed in LIFO order (Last In, First Out)
+
+	defer fmt.Println("I'm defer 1") // Will execute last
 	defer fmt.Println("I'm defer 2")
-	defer fmt.Println("I'm defer 3")
+	defer fmt.Println("I'm defer 3") // Will execute first among these three
 
 	fmt.Println("Defer in golang")
 
+	// Call function to demonstrate defer inside loops
 	myDefer()
 }
 
 func myDefer() {
+	// Loop from 0 to 4 (range over integer literal)
 	for i := range 5 {
+		// Each iteration registers a deferred call
+		// These will execute after the function completes, in reverse order
 		defer fmt.Println(i)
 	}
+
+	// At this point, nothing has been printed from the loop yet
+	// All deferred calls will run after this function returns
 }
