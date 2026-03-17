04b6614aa8ea794668a59075e179aacdeadab5d3
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-17 18:09:18 +0600

    feat(memory): demonstrate pointer operations
    - Show default nil pointer value
    - Illustrate pointer to variable
    - Demonstrate pointer dereferencing
    - Update variable via pointer

--- 07_Memory-Management/main.go
+++ 07_Memory-Management/main.go
@@ -6,17 +6,24 @@ import (
 )
 
 func main() {
+	// Print the number of logical CPUs available on the machine
 	fmt.Println("Logical CPUs:", runtime.NumCPU())
 
+	// Pointer declaration without initialization defaults to nil
 	var userName *string
-	fmt.Println("Default value of pointer is ", userName)
+	fmt.Println("Default value of pointer is", userName)
 
+	// Normal variable
 	myAge := 17
 
+	// Pointer to the variable myAge
 	var ptr = &myAge
-	fmt.Println("Memory address of pointer is: ", ptr)
-	fmt.Println("Actual value of pointer is: ", *ptr)
+	fmt.Println("Memory address of pointer is:", ptr)
 
+	// Dereferencing the pointer to get actual value
+	fmt.Println("Actual value of pointer is:", *ptr)
+
+	// Modify the value of myAge via pointer
 	*ptr = *ptr * 2
-	fmt.Println("My new age is: ", myAge) // i'm not this much old 😂️
+	fmt.Println("My new age is:", myAge) // I'm not this much old 😂️
 }
