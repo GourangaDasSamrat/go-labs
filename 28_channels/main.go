812fea2ad1a3fac11e9dea11fca6b759f04139f3
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-14 11:24:33 +0600

    feat(channels): implement select statement example
    - Demonstrates using select with two channels
    - Receives values from either chan1 or chan2
    - Iterates twice to receive from each channel
    - Replaces commented-out synchronization task

--- 28_channels/main.go
+++ 28_channels/main.go
@@ -26,18 +26,76 @@ func sum(result chan int, num1 int, num2 int) {
 */
 
 // synchronization
+/*
 func task(done chan bool) {
 	defer func() { done <- true }()
 
 	fmt.Println("Processing task...")
 }
+*/
+
+// email sender worker
+/*
+func emailSender(emailChan chan string, done chan bool) {
+	defer func() { done <- true }()
+
+	for email := range emailChan {
+		fmt.Println("Sending mail to", email)
+		time.Sleep(5 * time.Nanosecond)
+	}
+}
+*/
 
 func main() {
-	done := make(chan bool)
+	chan1 := make(chan int)
+	chan2 := make(chan string)
 
-	go task(done)
+	go func() {
+		chan1 <- 10
+	}()
 
-	<-done
+	go func() {
+		chan2 <- "Hello"
+	}()
+
+	for range 2 {
+		select {
+		case chan1Val := <-chan1:
+			fmt.Println(chan1Val)
+
+		case chan2Val := <-chan2:
+			fmt.Println(chan2Val)
+		}
+	}
+
+	/*
+		// buffered channel
+		emailChan := make(chan string, 100)
+		done := make(chan bool) // synchronizer channel
+
+		go emailSender(emailChan, done)
+
+		for range 100 {
+			local := make([]byte, 5)
+			for j := range local {
+				local[j] = byte(rand.Intn(26) + 'a')
+			}
+			email := string(local) + "@gmail.com"
+			emailChan <- email
+		}
+		close(emailChan)
+
+		<-done // block
+
+	*/
+
+	/*
+		done := make(chan bool)
+
+		go task(done)
+
+		<-done
+	*/
 
 	/*
 		result := make(chan int)
